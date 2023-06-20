faa5d9be0684d5622fa765938d89381b16288f4a
umeratiq99 <umer.atiq@focusteck.com>
2023-06-20 16:47:47 +0500

    Seperated the Controllers for Routes

---
+++ controllers/controllers.js
@@ -0,0 +1,58 @@
+const Users = require("../models/userTable");
+const { Op } = require("sequelize");
+
+const getData=async (req, res) => {
+    try {
+      let options = {};
+
+      if (req.query.skip && !req.query.take) {
+        throw new Error("Cannot offset Data");
+      }
+
+      if (req.query.take) {
+        options.limit = req.query.take;
+      }
+      if (req.query.skip) {
+        options.offset = req.query.skip;
+      }
+      if (req.query.order) {
+        {
+          options.order = [[req.query.orderby || "fname", req.query.order]];
+        }
+      }
+      if(req.query.fname || req.query.lname || req.query.desc){
+        let arr=[];
+      if(req.query.fname){
+        arr.push({fname: {[Op.iLike]: `%${req.query.fname}%`}});
+      }
+      if(req.query.lname){
+        arr.push({lname: {[Op.iLike]: `%${req.query.lname}%`}});
+      }
+      if(req.query.description){
+        arr.push({description: {[Op.iLike]: `%${req.query.description}%`}});
+      }
+      options.where={[Op.or]:arr};
+    }
+      const users = await Users.findAndCountAll(options);
+      const pages = parseInt(users.count);
+      res.send({
+        content: users.rows,
+        Count: pages,
+      });
+    } catch (err) {
+      res.send(err.message);
+    }
+  };
+
+  const deleteAll=async (req, res) => {
+    try {
+      const deleted = await Users.destroy({
+        where: {},
+      });
+      res.json("All Data Deleted");
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  module.exports={ getData , deleteAll }
--- router/userRoutes.js
+++ router/userRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Users = require("../models/userTable");
-const { Op } = require("sequelize");
 const { query, validationResult } = require("express-validator");
+const { getData, deleteAll } = require("../controllers/controllers");
 
 const validateResult = (req, res, next) => {
   const error = validationResult(req);
@@ -14,130 +13,79 @@ const validateResult = (req, res, next) => {
   }
 };
 
+const validations = [
+  query("skip")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("skip cannot be empty")
+    .isInt({ min: 0 })
+    .withMessage("Skip should be an Integer"),
+  query("take")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("take cannot be empty")
+    .isInt({ min: 0 })
+    .withMessage("Take should be an Integer"),
+  query("order")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("order cannot be empty")
+    .isString()
+    .withMessage("order should be string")
+    .custom((value) => {
+      if (value !== "ASC" && value !== "DESC") {
+        throw new Error(
+          "Incorrect Value for order: ASC for ascending and DESC for Descending"
+        );
+      }
+      return true;
+    }),
+  query("orderby")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("orderby cannot be empty")
+    .isString()
+    .withMessage("orderby should be string")
+    .custom((value) => {
+      if (value !== "fname" && value !== "createdAt") {
+        throw new Error(
+          "Incorrect Value for orderby: fname for User's first name and creatAt for Ordering by date"
+        );
+      }
+      return true;
+    }),
+  query("fname")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("fname cannot be empty")
+    .isString()
+    .withMessage("fname should be a String"),
+  query("lname")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("lname cannot be empty")
+    .isString()
+    .withMessage("lname should be a String"),
+  query("description")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("description cannot be empty")
+    .isString()
+    .withMessage("description should be a String"),
+];
+
 
 //Single API for Sorting Searching And Pagination
-router.get(
-  "/",
-  [
-    query("skip")
-      .optional()
-      .trim()
-      .notEmpty()
-      .withMessage("skip cannot be empty")
-      .isInt({ min: 0 })
-      .withMessage("Skip should be an Integer"),
-    query("take")
-      .optional()
-      .trim()
-      .notEmpty()
-      .withMessage("take cannot be empty")
-      .isInt({ min: 0 })
-      .withMessage("Take should be an Integer"),
-    query("order")
-      .optional()
-      .trim()
-      .notEmpty()
-      .withMessage("order cannot be empty")
-      .isString()
-      .withMessage("order should be string")
-      .custom((value) => {
-        if (value !== "ASC" && value !== "DESC") {
-          throw new Error(
-            "Incorrect Value for order: ASC for ascending and DESC for Descending"
-          );
-        }
-        return true;
-      }),
-    query("orderby")
-      .optional()
-      .trim()
-      .notEmpty()
-      .withMessage("orderby cannot be empty")
-      .isString()
-      .withMessage("orderby should be string")
-      .custom((value) => {
-        if (value !== "fname" && value !== "createdAt") {
-          throw new Error(
-            "Incorrect Value for orderby: firstname for User's first name and createddate for Ordering by date"
-          );
-        }
-        return true;
-      }),
-    query("fname")
-      .optional()
-      .trim()
-      .notEmpty()
-      .withMessage("fname cannot be empty")
-      .isString()
-      .withMessage("fname should be a String"),
-    query("lname")
-      .optional()
-      .trim()
-      .notEmpty()
-      .withMessage("lname cannot be empty")
-      .isString()
-      .withMessage("lname should be a String"),  
-    query("description")
-      .optional()
-      .trim()
-      .notEmpty()
-      .withMessage("description cannot be empty")
-      .isString()
-      .withMessage("description should be a String"),
-  ],
-  validateResult,
-  async (req, res) => {
-    try {
-      let options = {};
+router.get("/", validations, validateResult, getData);
 
-      if (req.query.skip && !req.query.take) {
-        throw new Error("Cannot offset Data");
-      }
+//Delete All data in DB
+router.delete("/", deleteAll);
 
-      if (req.query.take) {
-        options.limit = req.query.take;
-      }
-      if (req.query.skip) {
-        options.offset = req.query.skip;
-      }
-      if (req.query.order) {
-        {
-          options.order = [[req.query.orderby || "fname", req.query.order]];
-        }
-      }
-      if(req.query.fname || req.query.lname || req.query.desc){
-        let arr=[];
-      if(req.query.fname){
-        arr.push({fname: {[Op.like]: `%${req.query.fname}%`}});
-      }
-      if(req.query.lname){
-        arr.push({lname: {[Op.like]: `%${req.query.lname}%`}});
-      }
-      if(req.query.description){
-        arr.push({description: {[Op.like]: `%${req.query.description}%`}});
-      }
-      options.where={[Op.or]:arr};
-    }
-      const users = await Users.findAndCountAll(options);
-      const pages = parseInt(users.count);
-      res.send({
-        content: users.rows,
-        Count: pages,
-      });
-    } catch (err) {
-      res.send(err.message);
-    }
-  }
-);
-
-router.delete("/", async (req, res) => {
-  try {
-    const deleted = await Users.destroy({
-      where: {},
-    });
-    res.json("All Data Deleted");
-  } catch (err) {
-    console.error(err.message);
-  }
-});
 module.exports = router;
